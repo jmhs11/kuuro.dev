78c4361c2fe78acc5390f5b9113a6336103e4277
jmhs11 <josemariahs@gmail.com>
2022-06-05 19:32:32 +0200

    Create Footer and Header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+	return (
+		<footer className='flex justify-center pt-10 pb-6 md:container'>
+			<p>
+				Creado con ❤️ por{' '}
+				<a
+					href='https://github.com/jmhs11'
+					target='_blank'
+					rel='noopener noreferrer'
+          className="font-bold"
+				>
+					Kuuro
+				</a>
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;

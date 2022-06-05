de67c0428e02adbec33a2e03a0c3710f1cfdb231
jmhs11 <josemariahs@gmail.com>
2022-06-05 19:34:49 +0200

    Add loading to img

--- src/components/GridTechnology.jsx
+++ src/components/GridTechnology.jsx
@@ -7,7 +7,12 @@ const GridTechnology = ({ url, icon, altText }) => {
 			className='flex justify-center w-full p-3 bg-white border-2 rounded-md h-fit'
 			title={altText.split(' ')[0]}
 		>
-			<img src={`/icons/${icon}.svg`} alt={altText} className='w-20 h-20' />
+			<img
+				src={`/icons/${icon}.svg`}
+				alt={altText}
+				className='w-20 h-20'
+				loading='eager'
+			/>
 		</a>
 	);
 };

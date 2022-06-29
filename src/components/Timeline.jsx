e602d4b2a6e0cfde061942b359137f312a249f76
jmhs11 <josemariahs@gmail.com>
2022-06-29 22:11:28 +0200

    CV up

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,21 @@
+import TimelineItem from './TimelineItem';
+
+const Timeline = ({ timeline }) => {
+	return (
+		<div className='before:bg-[#333] before:w-1 before:h-full before:absolute before:left-1/2 before:-mt-8 before:ml-[-2px]'>
+			{timeline.map((t, index) => {
+        console.log(t);
+				return (
+					<TimelineItem
+						key={index}
+						company={t.company}
+						jobs={t.jobs}
+						index={index}
+					/>
+				);
+			})}
+		</div>
+	);
+};
+
+export default Timeline;
--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -1,18 +1,35 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Timeline from '../components/Timeline';
+import info from '../data/cv.json';
+
+
 
 const CV = () => {
 	return (
-    <>
+		<>
 			<Header />
 			<main>
 				<section className='px-6 py-56 md:container'>
-					Curriculum Vitae
+					<h1 className='text-4xl font-bold text-center'>Curriculum Vitae</h1>
+				</section>
+
+				<section id='timeline' className='px-6 py-8 mb-20  md:container'>
+					<Timeline timeline={info.experience}/>
+				</section>
+
+				<section className='px-6 pb-20 text-center md:container'>
+					<button
+						className='px-4 py-2 bg-blue-500 rounded-md'
+						onClick={() => alert('Generando PDF')}
+					>
+						Genera el documento PDF
+					</button>
 				</section>
 			</main>
 			<Footer />
 		</>
-  )
+	);
 };
 
 export default CV;

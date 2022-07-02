a52fcba469cc99dfe84ad1899b04ff7499949df6
jmhs11 <josemariahs@gmail.com>
2022-07-02 17:29:09 +0200

    Add some changes to hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,35 +11,47 @@ const Home = () => {
 		<>
 			<Header />
 			<main>
-				<section className='px-6 py-56 md:container'>
-					<h1 className='pb-2 font-bold'>Hola! soy Kuuro</h1>
-					<h3 className='text-[#6A6D7C] dark:text-[#ccc] pb-2 font-bold'>
-						Desarrollador Frontend
-					</h3>
-					<p className='text-[#6A6D7C] dark:text-[#ccc] pb-4'>
-						Soy Frontend developer con más de 2 años en el mundo de la
-						programación, actualmente aprendiendo de forma autodidacta React
-						&amp; Node.js y entusiasta de la cultura japonesa{' '}
-						<img
-							src='/icons/japan.png'
-							alt='Japan Flag'
-							className='inline w-4 h-4'
-						/>
-						⛩
-					</p>
-					<div className='flex gap-4'>
-						<Link
-							to='/cv'
-							className='px-4 py-2 text-white bg-blue-500 rounded-md'
-						>
-							Ver mi CV
-						</Link>
-						<Link
-              to="/contact"
-							className='px-4 py-2 text-white rounded-md bg-slate-600'
-						>
-							Contáctame
-						</Link>
+				<section className='flex flex-wrap items-center justify-between px-6 py-56 md:container'>
+					<div>
+						<h1 className='pb-2 font-bold'>Hola! soy Kuuro</h1>
+						<h3 className='text-[#6A6D7C] dark:text-[#ccc] pb-2 font-bold'>
+							Desarrollador Frontend
+						</h3>
+						<p className='text-[#6A6D7C] dark:text-[#ccc] pb-4 max-w-prose'>
+							Soy Frontend developer con más de 2 años en el mundo de la
+							programación, actualmente aprendiendo de forma autodidacta React
+							&amp; Node.js y entusiasta de la cultura japonesa{' '}
+							<img
+								src='/icons/japan.png'
+								alt='Japan Flag'
+								className='inline w-4 h-4'
+							/>
+							⛩
+						</p>
+						<div className='flex gap-4'>
+							<Link
+								to='/cv'
+								className='px-4 py-2 text-white bg-blue-500 rounded-md'
+							>
+								Ver mi CV
+							</Link>
+							<Link
+								to='/contact'
+								className='px-4 py-2 text-white rounded-md bg-slate-600'
+							>
+								Contáctame
+							</Link>
+						</div>
+					</div>
+					<div className='flex justify-center w-full sm:justify-around sm:w-auto'>
+						<picture>
+							<source type='image/webp' srcSet='hero.webp' />
+							<img
+								src='hero.jpg'
+								className='w-64 h-64 rounded-full'
+								alt='Kuuro Image'
+							/>
+						</picture>
 					</div>
 				</section>
 				<section className='px-6 pb-20 md:container'>

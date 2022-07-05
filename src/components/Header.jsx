4330f67a37b962f472722a981287d78cd697f4d0
jmhs11 <josemariahs@gmail.com>
2022-07-05 18:16:23 +0200

    test safari

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
 	return (
 		<header className='flex justify-between w-full h-20 px-6 md:container'>
 			<Link to='/'>
-				<img src='/logo.svg' alt='logo' className='h-full w-fit' />
+				<img src='/logo.svg' alt='logo' className='w-auto h-full' />
 			</Link>
 			<div id='social-media' className='flex items-center gap-2'>
 				<a

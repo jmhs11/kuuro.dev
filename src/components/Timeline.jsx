58f70bf9c36248c1caad386cb04dcada230ff7e2
jmhs11 <josemariahs@gmail.com>
2022-07-10 19:01:38 +0200

    Update TimelineItem

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,18 +3,16 @@ import TimelineItem from './TimelineItem';
 const Timeline = ({ timeline }) => {
 	return (
 		<div
-			className={`before:absolute md:before:left-1/2 before:-mt-8 before:ml-[-2px] before:h-full before:w-1 before:bg-[#333]`}
+			className={`before:absolute before:-mt-8 before:ml-[-2px] before:h-full before:w-1 before:bg-[#333] md:before:left-1/2`}
 		>
-			{timeline.map((t, index) => {
-				return (
-					<TimelineItem
-						key={index}
-						company={t.company}
-						jobs={t.jobs}
-						index={index}
-					/>
-				);
-			})}
+			{timeline.map((t, index) => (
+				<TimelineItem
+					key={index}
+					company={t.company}
+					jobs={t.jobs}
+					index={index}
+				/>
+			))}
 		</div>
 	);
 };

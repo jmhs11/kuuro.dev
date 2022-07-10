b51cc76616fdee2ee78f1ba75d5ff75c85ba8d2e
jmhs11 <josemariahs@gmail.com>
2022-07-10 19:04:33 +0200

    Add Contact component to CV and studies and skills sections

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -1,3 +1,4 @@
+import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Timeline from '../components/Timeline';
@@ -14,7 +15,15 @@ const CV = () => {
 
 				<section id='sobre-mi'>
 					<h2 className='mb-4 text-2xl font-bold text-center'>Sobre Mi</h2>
-					<div className='relative px-6 py-8 mb-20 md:container'></div>
+					<div className='px-6 py-8 mb-20 md:container'>
+						<p className='mx-auto text-center'>
+							Soy Frontend Developer con más de 3 años en el mundo de la
+							programación, me considero una persona muy curiosa, proactiva y me
+							encanta aprender cosas nuevas, por ello sigo aprendiendo de forma
+							autodidacta desarrollando proyectos asegurándome de garantizar la
+							mayor calidad posible en el producto final.
+						</p>
+					</div>
 				</section>
 
 				<section id='timeline'>
@@ -28,7 +37,32 @@ const CV = () => {
 
 				<section id='studies'>
 					<h2 className='mb-4 text-2xl font-bold text-center'>Estudios</h2>
-					<div className='relative px-6 py-8 mb-20 md:container'></div>
+					<div className='px-6 py-8 mb-20 md:container'>
+						{info.education.map((studies, index) => (
+							<div key={index}>
+								<small>
+									{new Date(studies.startDate).toLocaleString('es-ES', {
+										month: 'short',
+										year: 'numeric'
+									})}{' '}
+									-{' '}
+									{studies.endDate
+										? ` ${new Date(studies.endDate).toLocaleString('es-ES', {
+												month: 'short',
+												year: 'numeric'
+										  })}`
+										: 'Actualmente'}
+								</small>
+								<p>{studies.title}</p>
+								<p>{studies.institution}</p>
+								<p>{studies.certificate}</p>
+							</div>
+						))}
+					</div>
+				</section>
+
+				<section className='px-6 pb-20 md:container'>
+					<h2 className='mb-4 text-2xl font-bold text-center'>Skills</h2>
 				</section>
 
 				<section className='px-6 pb-20 text-center md:container'>
@@ -40,6 +74,7 @@ const CV = () => {
 						Descargar CV
 					</a>
 				</section>
+				<Contact />
 			</main>
 			<Footer />
 		</>

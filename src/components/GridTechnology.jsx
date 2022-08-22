65b4ef4a3ae89042ea0652d403f796bcaf27234e
jmhs11 <josemariahs@gmail.com>
2022-08-23 00:59:04 +0200

    Fix PNG icons styles in mobile

--- src/components/GridTechnology.jsx
+++ src/components/GridTechnology.jsx
@@ -4,13 +4,17 @@ const GridTechnology = ({ url, icon, isPNG, altText, className }) => {
 			href={url}
 			rel='nooppener noreferrer'
 			target='_blank'
-			className='flex h-fit w-full justify-center rounded-md border-2 bg-white p-3 dark:border-[#333] dark:bg-black'
+			className={`flex ${
+				isPNG ? 'h-full md:h-min' : 'h-fit'
+			} w-full justify-center rounded-md border-2 bg-white p-3 dark:border-[#333] dark:bg-black`}
 			title={altText.split(' ')[0]}
 		>
 			<img
 				src={`/icons/${icon}.${isPNG ? 'png' : 'svg'}`}
 				alt={altText}
-				className={`h-20 w-20 ${className}`}
+				className={`${
+					isPNG ? 'my-auto h-fit w-full md:h-20 md:object-contain' : 'h-20 w-20'
+				} ${className}`}
 				loading='eager'
 			/>
 		</a>
